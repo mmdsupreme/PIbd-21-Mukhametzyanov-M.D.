91659845a1673aae484c757d334ae15b3d2d4007
mmdsupreme <31138261+mmdsupreme@users.noreply.github.com>
2018-01-24 16:04:50 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace WindowsFormsLab2
+namespace WindowsFormsLab3
 {
-    public static class Program
+    static class Program
     {
         /// <summary>
         /// Главная точка входа для приложения.
@@ -16,7 +16,7 @@ namespace WindowsFormsLab2
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FormParking());
         }
     }
 }
--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace WindowsFormsLab2.Properties
+namespace WindowsFormsLab3.Properties
 {
 
 
@@ -44,7 +44,7 @@ namespace WindowsFormsLab2.Properties
             {
                 if ((resourceMan == null))
                 {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("WindowsFormsLab2.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("WindowsFormsLab3.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
