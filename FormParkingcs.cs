bcf183ef1d3438d72e41eaf9f58cebd4ba7d1032
mmdsupreme <31138261+mmdsupreme@users.noreply.github.com>
2018-01-25 09:58:09 +0300

    Add files via upload

---
+++ FormParkingcs.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab2
+{
+    public partial class FormParkingcs : Form
+    {
+        Parking parking;
+        Form1 form;
+        public FormParkingcs()
+        {
+            InitializeComponent();
+            parking = new Parking(5);
+            for (int i = 1; i < 6; i++)
+            {
+                listBoxLevels.Items.Add("Уровень " + i);
+            }
+            listBoxLevels.SelectedIndex = parking.getCurrentLevel;
+   
+            Draw();
+        }
+
+        private void Draw()
+        {
+            if (listBoxLevels.SelectedIndex > -1)
+            {
+                Bitmap bmp = new Bitmap(pictureBoxParking.Width, pictureBoxParking.Height);
+                Graphics gr = Graphics.FromImage(bmp);
+                parking.Draw(gr, pictureBoxParking.Width, pictureBoxParking.Height);
+                pictureBoxParking.Image = bmp;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ColorDialog dialog = new ColorDialog();
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                var car = new Car(100, 4, 1000, dialog.Color);
+                int place = parking.PutCarInParking(car);
+                Draw();
+                MessageBox.Show("Ваше место: " + place);
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            {
+                ColorDialog dialog = new ColorDialog();
+                if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    ColorDialog dialogDop = new ColorDialog();
+                    if (dialogDop.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                    {
+                        var car = new Jeep(100, 4, 1000, dialog.Color, true, true, true, dialogDop.Color);
+                        int place = parking.PutCarInParking(car);
+                        Draw();
+                        MessageBox.Show("Ваше место: " + place);
+                    }
+                }
+
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            {
+                if (listBoxLevels.SelectedIndex > -1)
+                {//Прежде чем забрать машину, надо выбрать с какого уровня будем забирать
+                    string level = listBoxLevels.Items[listBoxLevels.SelectedIndex].ToString();
+                    if (maskedTextBox1.Text != "")
+                    {
+                        var car = parking.GetCarInParking(Convert.ToInt32(maskedTextBox1.Text));
+
+                        Bitmap bmp = new Bitmap(pictureBoxTakeCar.Width, pictureBoxTakeCar.Height);
+                        Graphics gr = Graphics.FromImage(bmp);
+                        car.setPosition(20, 50);
+                        car.drawCar(gr);
+                        pictureBoxTakeCar.Image = bmp;
+                        Draw();
+                    }
+                    else
+                    {//иначесообщаемобэтом
+                        MessageBox.Show("Извинте, на этом месте нет машины");
+                    }
+
+                }
+            }
+        }
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonDown_Click(object sender, EventArgs e)
+        {
+            parking.LevelDown();
+            listBoxLevels.SelectedIndex = parking.getCurrentLevel;
+            Draw();
+
+        }
+
+        private void buttonUp_Click(object sender, EventArgs e)
+        {
+            parking.LevelUp();
+            listBoxLevels.SelectedIndex = parking.getCurrentLevel;
+            Draw();
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            form = new Form1();
+            form.AddEvent(AddLoco);
+            form.Show();
+
+        }
+
+        private void AddLoco(ITransport car)
+        {
+            if (car != null)
+            {
+                int place = parking.PutCarInParking(car);
+                if (place > -1)
+                {
+                    Draw();
+                    MessageBox.Show("Ваше место: " + place);
+                }
+                else
+                {
+                    MessageBox.Show("Машину не удалось поставить");
+                }
+            }
+        }
+
+    }
+}
+
---
+++ Jeep.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    public class Jeep : Car
+    {
+        private bool suspension;
+        private bool reserve;
+        private bool exhaustPipe;
+        private Color dopColor;
+
+        public Jeep(int maxSpeed, int maxCountPassengers, double weight, Color color, bool suspension, bool reserve, bool exhaustPipe, Color dopColor) : base(maxSpeed, maxCountPassengers, weight, color)
+        {
+            this.suspension = suspension;
+            this.reserve = reserve;
+            this.exhaustPipe = exhaustPipe;
+            this.dopColor = dopColor;
+        }
+
+        protected override void drawBaseCar(Graphics g)
+        {
+            if (suspension)
+            {
+                Pen pen = new Pen(Color.Black);
+                g.DrawEllipse(pen, startPosX + 60, startPosY - 6, 20, 20);
+                g.DrawEllipse(pen, startPosX + 60, startPosY + 35, 20, 20);
+                g.DrawEllipse(pen, startPosX + 8, startPosY - 6, 20, 20);
+                g.DrawEllipse(pen, startPosX + 8, startPosY + 35, 20, 20);
+                g.DrawRectangle(pen, startPosX + 80, startPosY + 10, 15, 20);
+                g.DrawRectangle(pen, startPosX + 80, startPosY + 27, 15, 20);
+                g.DrawRectangle(pen, startPosX, startPosY - 6, 14, 15);
+                g.DrawRectangle(pen, startPosX, startPosY + 40, 14, 15);
+
+                Brush suspension = new SolidBrush(dopColor);
+
+
+                g.FillRectangle(suspension, startPosX, startPosY - 5, 15, 15);
+                g.FillRectangle(suspension, startPosX, startPosY + 40, 15, 15);
+            }
+
+
+
+            if (reserve)
+            {
+                Brush spoiler = new SolidBrush(dopColor);
+                g.FillRectangle(spoiler, startPosX - 10, startPosY, 10, 30);
+                Pen pen = new Pen(Color.Black);
+                g.DrawRectangle(pen, startPosX - 5, startPosY, 10, 50);
+
+            }
+            if (exhaustPipe)
+            {
+                Pen pen = new Pen(Color.Black);
+
+                g.DrawRectangle(pen, startPosX + 25, startPosY + 50, 10, 10);
+
+                Brush pipe = new SolidBrush(dopColor);
+
+                g.FillRectangle(pipe, startPosX + 25, startPosY + 45, 10, 15);
+
+            }
+            base.drawBaseCar(g);
+            Brush br = new SolidBrush(dopColor);
+
+
+
+            g.FillRectangle(br, startPosX + 15, startPosY + 15, 35, 5);
+            g.FillRectangle(br, startPosX + 15, startPosY + 30, 35, 5);
+
+        }
+
+        public void setDopColor(Color color)
+        {
+            dopColor = color;
+        }
+
+
+    }
+}
