27a1b46afb38b13349532f8a894ed6228c4cc2ec
mmdsupreme <31138261+mmdsupreme@users.noreply.github.com>
2018-01-31 23:52:20 +0300

    Add files via upload

---
+++ Lab1/Salt.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ULSTU_OOP_Csharp_HomeLab1
+{
+    class Salt
+    {
+        public double Count
+        {
+            set;
+            get;
+        }
+    }
+}
---
+++ forcemeat.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    class forcemeat
+    {
+        private int temperature = 0;
+
+        public double Count
+        {
+            set;
+            get;
+        }
+
+        public int Temperature
+        {
+            get
+            {
+                return temperature;
+            }
+        }
+
+        public void GetHeat()
+        {
+            if (temperature < 100)
+            {
+                temperature++;
+            }
+        }
+
+        public bool State
+        {
+            set;
+            get;
+        }
+
+        public forcemeat GetOil()
+        {
+            if (State)
+            {
+                return new forcemeat();
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
