aa445e91426f0f8f85e01be9ef18500e2771f568
mmdsupreme <31138261+mmdsupreme@users.noreply.github.com>
2018-02-01 09:03:29 +0300

    Add files via upload

---
+++ potato.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    class potato
+    {
+        private int has_ready = 0;
+        private int dirty = 10;
+
+        public bool Have_shell
+        {
+            set;
+            get;
+        }
+
+        public int Has_ready
+        {
+            get
+            {
+                return has_ready;
+            }
+        }
+
+      
+
+        public void GetHeat()
+        {
+            if (has_ready < 10)
+            {
+                has_ready++;
+            }
+        }
+
+        public int Dirty
+        {
+            set
+            {
+                if (value > -1 && value < 11) dirty = value;
+            }
+            get
+            {
+                return dirty;
+            }
+        }
+
+    }
+}
